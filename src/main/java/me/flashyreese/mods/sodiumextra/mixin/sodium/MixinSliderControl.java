f88c7f79f7f1d99514e62b99c9c6a4e12ce12afd
Yao Chung Hu <30311066+FlashyReese@users.noreply.github.com>
2021-02-24 14:21:09 -0600

    new: Adjust maximum brightness slider

---
+++ src/main/java/me/flashyreese/mods/sodiumextra/mixin/sodium/MixinSliderControl.java
@@ -0,0 +1,30 @@
+package me.flashyreese.mods.sodiumextra.mixin.sodium;
+
+import me.jellysquid.mods.sodium.client.gui.options.Option;
+import me.jellysquid.mods.sodium.client.gui.options.control.ControlValueFormatter;
+import me.jellysquid.mods.sodium.client.gui.options.control.SliderControl;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(SliderControl.class)
+public class MixinSliderControl {
+    @Shadow
+    @Final
+    @Mutable
+    private int max;
+
+    /*
+     * Completely sane
+     */
+    @Inject(method = "<init>", at = @At(value = "TAIL"))
+    public void init(Option<Integer> option, int min, int max, int interval, ControlValueFormatter mode, CallbackInfo ci){
+        if (mode == ControlValueFormatter.brightness()){
+            this.max = 1000;
+        }
+    }
+}
