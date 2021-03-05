7f93ae9573df90453c5faea60f4245e3bc885c23
Yao Chung Hu <30311066+FlashyReese@users.noreply.github.com>
2021-03-05 15:59:58 -0600

    change: Correct injection points
    Closes #13

--- src/main/java/me/flashyreese/mods/sodiumextra/mixin/features/particle/MixinWorldRenderer.java
+++ src/main/java/me/flashyreese/mods/sodiumextra/mixin/features/particle/MixinWorldRenderer.java
@@ -16,14 +16,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(WorldRenderer.class)
 public class MixinWorldRenderer {
 
-    @Inject(method = "tickRainSplashing", at = @At(value = "INVOKE"), cancellable = true)
+    @Inject(method = "tickRainSplashing", at = @At(value = "HEAD"), cancellable = true)
     public void tickRainSplashing(Camera camera, CallbackInfo callbackInfo) {
         if (!(SodiumExtraClientMod.options().particleSettings.particles && SodiumExtraClientMod.options().particleSettings.rainSplash)) {
             callbackInfo.cancel();
         }
     }
 
-    @Inject(method = "renderWeather", at = @At(value = "INVOKE"), cancellable = true)
+    @Inject(method = "renderWeather", at = @At(value = "HEAD"), cancellable = true)
     private void renderWeather(LightmapTextureManager manager, float f, double d, double e, double g, CallbackInfo callbackInfo) {
         if (!(SodiumExtraClientMod.options().detailSettings.rainSnow)) {
             callbackInfo.cancel();
