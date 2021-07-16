1c10199922b31a7fafc59e1deb408e4647b8c45c
Madis <Madis0@users.noreply.github.com>
2021-07-16 22:25:46 +0300

    Vanilla strings for fog

--- src/main/java/me/flashyreese/mods/sodiumextra/client/gui/ControlValueFormatterExtended.java
+++ src/main/java/me/flashyreese/mods/sodiumextra/client/gui/ControlValueFormatterExtended.java
@@ -1,19 +1,18 @@
 package me.flashyreese.mods.sodiumextra.client.gui;
 
 import me.jellysquid.mods.sodium.client.gui.options.control.ControlValueFormatter;
+import net.minecraft.text.TranslatableText;
 
 public interface ControlValueFormatterExtended extends ControlValueFormatter {
 
     static ControlValueFormatter fogDistance() {
         return (v) -> {
             if (v == 0) {
-                return "Vanilla";
+                return new TranslatableText("generator.default").getString();
             } else if (v == 33) {
-                return "Off";
-            } else if (v == 1) {
-                return v + " Chunk";
+                return new TranslatableText("options.off").getString();
             } else {
-                return v + " Chunks";
+                return new TranslatableText("options.chunks", v).getString();
             }
         };
     }
