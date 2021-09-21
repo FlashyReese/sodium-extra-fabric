6a57e57c3fbeef84a706c8970c93f18af3b7821e
Yao Chung Hu <30311066+FlashyReese@users.noreply.github.com>
2021-09-21 17:19:27 -0600

    change: Uncursify tabs when using Reese's Sodium Options

--- src/main/java/me/flashyreese/mods/sodiumextra/mixin/reeses_sodium_options/MixinSodiumVideoOptionsScreen.java
+++ src/main/java/me/flashyreese/mods/sodiumextra/mixin/reeses_sodium_options/MixinSodiumVideoOptionsScreen.java
@@ -1,23 +1,18 @@
 package me.flashyreese.mods.sodiumextra.mixin.reeses_sodium_options;
 
 import me.flashyreese.mods.reeses_sodium_options.client.gui.SodiumVideoOptionsScreen;
-import me.flashyreese.mods.reeses_sodium_options.client.gui.frame.BasicFrame;
-import me.flashyreese.mods.reeses_sodium_options.client.gui.frame.tab.Tab;
-import me.flashyreese.mods.reeses_sodium_options.client.gui.frame.tab.TabFrame;
 import me.flashyreese.mods.sodiumextra.client.gui.SodiumExtraGameOptionPages;
 import me.jellysquid.mods.sodium.client.gui.options.OptionFlag;
 import me.jellysquid.mods.sodium.client.gui.options.OptionPage;
 import me.jellysquid.mods.sodium.client.gui.options.storage.OptionStorage;
-import me.jellysquid.mods.sodium.client.gui.widgets.FlatButtonWidget;
-import me.jellysquid.mods.sodium.client.util.Dim2i;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.text.LiteralText;
-import net.minecraft.text.TranslatableText;
-import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import java.util.EnumSet;
@@ -27,12 +22,6 @@ import java.util.List;
 @Pseudo
 @Mixin(SodiumVideoOptionsScreen.class)
 public abstract class MixinSodiumVideoOptionsScreen {
-
-    @Shadow
-    private FlatButtonWidget applyButton, closeButton, undoButton;
-
-    @Shadow
-    private FlatButtonWidget donateButton, hideDonateButton;
 
     @Shadow
     @Final
@@ -45,51 +34,6 @@ public abstract class MixinSodiumVideoOptionsScreen {
         this.pages.add(SodiumExtraGameOptionPages.detail());
         this.pages.add(SodiumExtraGameOptionPages.render());
         this.pages.add(SodiumExtraGameOptionPages.extra());
-    }
-
-    @Unique
-    @Inject(method = "parentFrameBuilder",
-            at = @At(
-                    value = "INVOKE_ASSIGN",
-                    target = "Lme/flashyreese/mods/reeses_sodium_options/client/gui/frame/BasicFrame;createBuilder()Lme/flashyreese/mods/reeses_sodium_options/client/gui/frame/BasicFrame$Builder;"),
-            remap = false,
-            locals = LocalCapture.CAPTURE_FAILEXCEPTION,
-            cancellable = true
-    )
-    private void parentBasicFrameBuilder(CallbackInfoReturnable<BasicFrame.Builder> cir, Dim2i basicFrameDim, Dim2i tabFrameDim) {
-        BasicFrame.Builder builder = BasicFrame.createBuilder()
-                .setDimension(basicFrameDim)
-                .addChild(parentDim -> TabFrame.createBuilder()
-                        .setDimension(tabFrameDim)
-                        .addTabs(tabs -> this.pages.forEach(page -> {
-                            if (page.getName() instanceof TranslatableText translatableText) {
-                                if (!(translatableText.getKey().startsWith("sodium-extra") || translatableText.getKey().startsWith("options.particles"))) {
-                                    tabs.add(dim -> new Tab.Builder<>().from(page, dim));
-                                }
-                            }
-                        }))
-                        .addTab(subDim -> new Tab.Builder<>()
-                                .setText(new LiteralText("Sodium Extra"))
-                                .setFrame(TabFrame.createBuilder()
-                                        .setDimension(subDim)
-                                        .addTabs(subTabs -> this.pages.forEach(page -> {
-                                            if (page.getName() instanceof TranslatableText translatableText) {
-                                                if (translatableText.getKey().startsWith("sodium-extra") || translatableText.getKey().startsWith("options.particles")) {
-                                                    subTabs.add(subSubDim -> new Tab.Builder<>().from(page, subSubDim));
-                                                }
-                                            }
-                                        }))
-                                        .build())
-                                .build()
-                        )
-                        .build()
-                )
-                .addChild(dim -> this.undoButton)
-                .addChild(dim -> this.applyButton)
-                .addChild(dim -> this.closeButton)
-                .addChild(dim -> this.donateButton)
-                .addChild(dim -> this.hideDonateButton);
-        cir.setReturnValue(builder);
     }
 
     /*
