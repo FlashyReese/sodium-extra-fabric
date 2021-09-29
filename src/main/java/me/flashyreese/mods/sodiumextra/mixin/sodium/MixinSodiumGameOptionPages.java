128ca1ec08907b22aca851c7d71f7b44a8b1b1e3
Madis Otenurm <Madis0@users.noreply.github.com>
2021-09-29 05:03:01 +0300

    Improve resolution slider (#86)
    * Use vanilla string
    
    * Reorder
    
    * Remove it and ru strings

--- src/main/java/me/flashyreese/mods/sodiumextra/mixin/sodium/MixinSodiumGameOptionPages.java
+++ src/main/java/me/flashyreese/mods/sodiumextra/mixin/sodium/MixinSodiumGameOptionPages.java
@@ -26,11 +26,11 @@ public class MixinSodiumGameOptionPages {
     @Final
     private static MinecraftOptionsStorage vanillaOpts;
 
-    @Inject(method = "general", at = @At(value = "INVOKE", target = "Lme/jellysquid/mods/sodium/client/gui/options/OptionGroup;createBuilder()Lme/jellysquid/mods/sodium/client/gui/options/OptionGroup$Builder;", ordinal = 0, shift = At.Shift.BEFORE), locals = LocalCapture.CAPTURE_FAILSOFT, remap = false)
+    @Inject(method = "general", at = @At(value = "INVOKE", target = "Lme/jellysquid/mods/sodium/client/gui/options/OptionGroup;createBuilder()Lme/jellysquid/mods/sodium/client/gui/options/OptionGroup$Builder;", ordinal = 1, shift = At.Shift.BEFORE), locals = LocalCapture.CAPTURE_FAILSOFT, remap = false)
     private static void general(CallbackInfoReturnable<OptionPage> cir, List<OptionGroup> groups){
         groups.add(OptionGroup.createBuilder()
                 .add(OptionImpl.createBuilder(int.class, vanillaOpts)
-                        .setName(new TranslatableText("sodium-extra.option.resolution"))
+                        .setName(new TranslatableText("options.fullscreen.resolution"))
                         .setTooltip(new TranslatableText("sodium-extra.option.resolution.tooltip"))
                         .setControl(option -> new SliderControlExtended(option, 0, MinecraftClient.getInstance().getWindow().getMonitor() != null ? MinecraftClient.getInstance().getWindow().getMonitor().getVideoModeCount() : 0, 1, ControlValueFormatterExtended.resolution(), false))
                         .setBinding((options, value) -> {
