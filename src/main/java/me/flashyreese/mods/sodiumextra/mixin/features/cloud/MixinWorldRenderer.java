1d46c51d6aa36cfb5dc894ebf4805bd981cab014
DragonEggBedrockBreaking <cuber3million@gmail.com>
2021-11-19 23:21:16 +0000

    Update to 1.18.

--- src/main/java/me/flashyreese/mods/sodiumextra/mixin/features/cloud/MixinWorldRenderer.java
+++ src/main/java/me/flashyreese/mods/sodiumextra/mixin/features/cloud/MixinWorldRenderer.java
@@ -1,7 +1,7 @@
 package me.flashyreese.mods.sodiumextra.mixin.features.cloud;
 
 import me.flashyreese.mods.sodiumextra.client.SodiumExtraClientMod;
-import net.minecraft.client.render.SkyProperties;
+import net.minecraft.client.render.DimensionEffects;
 import net.minecraft.client.render.WorldRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,10 +12,10 @@ public class MixinWorldRenderer {
 
     @Redirect(
             method = "renderClouds(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/util/math/Matrix4f;FDDD)V",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/SkyProperties;getCloudsHeight()F")
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/DimensionEffects;getCloudsHeight()F")
     )
-    private float getCloudHeight(SkyProperties skyProperties) {
-        if (skyProperties.getSkyType() == SkyProperties.SkyType.NORMAL) {
+    private float getCloudHeight(DimensionEffects skyProperties) {
+        if (skyProperties.getSkyType() == DimensionEffects.SkyType.NORMAL) {
             return SodiumExtraClientMod.options().extraSettings.cloudHeight;
         }
         return skyProperties.getCloudsHeight();
