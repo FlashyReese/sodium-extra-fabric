b97d7d2d639049b4850d32b7604799379065a616
Yao Chung Hu <30311066+FlashyReese@users.noreply.github.com>
2022-01-21 19:21:31 +0800

    change: Move accessibility sliders to separate mixin package

---
+++ src/main/java/me/flashyreese/mods/sodiumextra/mixin/sodium/accessibility/MixinSodiumGameOptionPages.java
@@ -0,0 +1,50 @@
+package me.flashyreese.mods.sodiumextra.mixin.sodium.accessibility;
+
+import me.jellysquid.mods.sodium.client.gui.SodiumGameOptionPages;
+import me.jellysquid.mods.sodium.client.gui.options.OptionGroup;
+import me.jellysquid.mods.sodium.client.gui.options.OptionImpact;
+import me.jellysquid.mods.sodium.client.gui.options.OptionImpl;
+import me.jellysquid.mods.sodium.client.gui.options.OptionPage;
+import me.jellysquid.mods.sodium.client.gui.options.control.ControlValueFormatter;
+import me.jellysquid.mods.sodium.client.gui.options.control.SliderControl;
+import me.jellysquid.mods.sodium.client.gui.options.storage.MinecraftOptionsStorage;
+import net.minecraft.text.TranslatableText;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import java.util.List;
+
+@Mixin(SodiumGameOptionPages.class)
+public class MixinSodiumGameOptionPages {
+
+    @Shadow
+    @Final
+    private static MinecraftOptionsStorage vanillaOpts;
+
+    @Inject(method = "quality", at = @At(value = "INVOKE", target = "Lme/jellysquid/mods/sodium/client/gui/options/OptionGroup;createBuilder()Lme/jellysquid/mods/sodium/client/gui/options/OptionGroup$Builder;", ordinal = 2, shift = At.Shift.AFTER), locals = LocalCapture.CAPTURE_FAILSOFT, remap = false)
+    private static void quality(CallbackInfoReturnable<OptionPage> cir, List<OptionGroup> groups){
+        groups.add(OptionGroup.createBuilder()
+                .add(OptionImpl.createBuilder(int.class, vanillaOpts)
+                        .setName(new TranslatableText("options.screenEffectScale"))
+                        .setTooltip(new TranslatableText("options.screenEffectScale.tooltip"))
+                        .setControl(option -> new SliderControl(option, 0, 100, 1, ControlValueFormatter.percentage()))
+                        .setBinding((opts, value) -> opts.distortionEffectScale = (float) value / 100.0F, (opts) -> Math.round(opts.distortionEffectScale * 100.0F))
+                        .setImpact(OptionImpact.LOW)
+                        .build()
+                )
+                .add(OptionImpl.createBuilder(int.class, vanillaOpts)
+                        .setName(new TranslatableText("options.fovEffectScale"))
+                        .setTooltip(new TranslatableText("options.fovEffectScale.tooltip"))
+                        .setControl(option -> new SliderControl(option, 0, 100, 1, ControlValueFormatter.percentage()))
+                        .setBinding((opts, value) -> opts.fovEffectScale = (float) Math.sqrt(value / 100.0F), (opts) -> (int) Math.round(Math.pow(opts.fovEffectScale, 2.0D) * 100.0F))
+                        .setImpact(OptionImpact.LOW)
+                        .build()
+                )
+                .build());
+    }
+}
