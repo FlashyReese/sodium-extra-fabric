d02a1f1a1dc120c1250ea7c3d0cfbd0715e62b05
Yao Chung Hu <30311066+FlashyReese@users.noreply.github.com>
2022-01-25 22:08:58 +0800

    change: code improvements (#97)

--- src/main/java/me/flashyreese/mods/sodiumextra/common/util/ControlValueFormatterExtended.java
+++ src/main/java/me/flashyreese/mods/sodiumextra/common/util/ControlValueFormatterExtended.java
@@ -2,16 +2,18 @@ package me.flashyreese.mods.sodiumextra.common.util;
 
 import me.jellysquid.mods.sodium.client.gui.options.control.ControlValueFormatter;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.util.Monitor;
 import net.minecraft.text.LiteralText;
 import net.minecraft.text.TranslatableText;
 
 public interface ControlValueFormatterExtended extends ControlValueFormatter {
     static ControlValueFormatter resolution() {
+        Monitor monitor = MinecraftClient.getInstance().getWindow().getMonitor();
         return (v) -> {
-            if (MinecraftClient.getInstance().getWindow().getMonitor() == null) {
+            if (monitor == null) {
                 return new TranslatableText("options.fullscreen.unavailable").getString();
             } else {
-                return v == 0 ? new TranslatableText("options.fullscreen.current").getString() : new LiteralText(MinecraftClient.getInstance().getWindow().getMonitor().getVideoMode(v - 1).toString()).getString();
+                return v == 0 ? new TranslatableText("options.fullscreen.current").getString() : new LiteralText(monitor.getVideoMode(v - 1).toString()).getString();
             }
         };
     }
--- src/main/java/me/flashyreese/mods/sodiumextra/mixin/sodium/MixinSodiumGameOptionPages.java
+++ src/main/java/me/flashyreese/mods/sodiumextra/mixin/sodium/MixinSodiumGameOptionPages.java
@@ -7,6 +7,7 @@ import me.jellysquid.mods.sodium.client.gui.options.*;
 import me.jellysquid.mods.sodium.client.gui.options.storage.MinecraftOptionsStorage;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.util.VideoMode;
+import net.minecraft.client.util.Window;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -27,25 +28,27 @@ public class MixinSodiumGameOptionPages {
 
     @Inject(method = "general", at = @At(value = "INVOKE", target = "Lme/jellysquid/mods/sodium/client/gui/options/OptionGroup;createBuilder()Lme/jellysquid/mods/sodium/client/gui/options/OptionGroup$Builder;", ordinal = 0, shift = At.Shift.BEFORE), locals = LocalCapture.CAPTURE_FAILSOFT, remap = false)
     private static void general(CallbackInfoReturnable<OptionPage> cir, List<OptionGroup> groups){
+        Window window = MinecraftClient.getInstance().getWindow();
+
         groups.add(OptionGroup.createBuilder()
                 .add(OptionImpl.createBuilder(int.class, vanillaOpts)
                         .setName("Resolution")
                         .setTooltip("Sets resolution of the game.")
                         .setControl(option -> new SliderControlExtended(option, 0, MinecraftClient.getInstance().getWindow().getMonitor() != null ? MinecraftClient.getInstance().getWindow().getMonitor().getVideoModeCount() : 0, 1, ControlValueFormatterExtended.resolution(), false))
                         .setBinding((options, value) -> {
-                            if (MinecraftClient.getInstance().getWindow().getMonitor() != null) {
+                            if (window.getMonitor() != null) {
                                 if (value == 0) {
-                                    MinecraftClient.getInstance().getWindow().setVideoMode(Optional.empty());
+                                    window.setVideoMode(Optional.empty());
                                 } else {
-                                    MinecraftClient.getInstance().getWindow().setVideoMode(Optional.of(MinecraftClient.getInstance().getWindow().getMonitor().getVideoMode(value - 1)));
+                                    window.setVideoMode(Optional.of(window.getMonitor().getVideoMode(value - 1)));
                                 }
                             }
                         }, options -> {
-                            if (MinecraftClient.getInstance().getWindow().getMonitor() == null) {
+                            if (window.getMonitor() == null) {
                                 return 0;
                             } else {
-                                Optional<VideoMode> optional = MinecraftClient.getInstance().getWindow().getVideoMode();
-                                return optional.map((videoMode) -> MinecraftClient.getInstance().getWindow().getMonitor().findClosestVideoModeIndex(videoMode) + 1).orElse(0);
+                                Optional<VideoMode> optional = window.getVideoMode();
+                                return optional.map((videoMode) -> window.getMonitor().findClosestVideoModeIndex(videoMode) + 1).orElse(0);
                             }
                         })
                         .setFlags(OptionFlag.REQUIRES_GAME_RESTART)
