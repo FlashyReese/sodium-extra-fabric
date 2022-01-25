080dd0cff2347746931e8c673873a982a2593d2c
Yao Chung Hu <30311066+FlashyReese@users.noreply.github.com>
2022-01-25 23:14:57 +0800

    change: Mixin Package Refactoring

---
+++ src/main/java/me/flashyreese/mods/sodiumextra/mixin/entity/MixinPaintingEntityRenderer.java
@@ -0,0 +1,20 @@
+package me.flashyreese.mods.sodiumextra.mixin.entity;
+
+import me.flashyreese.mods.sodiumextra.client.SodiumExtraClientMod;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.PaintingEntityRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.decoration.painting.PaintingEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(PaintingEntityRenderer.class)
+public class MixinPaintingEntityRenderer {
+    @Inject(at = @At("HEAD"), method = "render(Lnet/minecraft/entity/decoration/painting/PaintingEntity;FFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V", cancellable = true)
+    public void render(PaintingEntity paintingEntity, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, CallbackInfo ci) {
+        if (!SodiumExtraClientMod.options().renderSettings.lightUpdates)
+            ci.cancel();
+    }
+}
---
+++ src/main/java/me/flashyreese/mods/sodiumextra/mixin/gui/MinecraftClientAccessor.java
@@ -0,0 +1,11 @@
+package me.flashyreese.mods.sodiumextra.mixin.gui;
+
+import net.minecraft.client.MinecraftClient;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(MinecraftClient.class)
+public interface MinecraftClientAccessor {
+    @Accessor("currentFps")
+    int getCurrentFPS();
+}
