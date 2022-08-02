2441a4160fda90e9ac8babe3e6090a37efb02970
Yao Chung Hu <30311066+FlashyReese@users.noreply.github.com>
2022-08-03 06:29:53 +0800

    change: Add firework particle toggle workaround

--- src/main/java/me/flashyreese/mods/sodiumextra/mixin/particle/MixinWorldRenderer.java
+++ src/main/java/me/flashyreese/mods/sodiumextra/mixin/particle/MixinWorldRenderer.java
@@ -30,16 +30,4 @@ public class MixinWorldRenderer {
             callbackInfo.cancel();
         }
     }
-
-    @Inject(method = "spawnParticle(Lnet/minecraft/particle/ParticleEffect;ZZDDDDDD)Lnet/minecraft/client/particle/Particle;", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/WorldRenderer;getRandomParticleSpawnChance(Z)Lnet/minecraft/client/option/ParticlesMode;"), cancellable = true)
-    private void spawnParticle(ParticleEffect parameters, boolean alwaysSpawn, boolean canSpawnOnMinimal, double x, double y, double z, double velocityX, double velocityY, double velocityZ, CallbackInfoReturnable<Particle> callbackInfo) {
-        if (SodiumExtraClientMod.options().particleSettings.particles) {
-            Identifier particleTypeId = Registry.PARTICLE_TYPE.getId(parameters.getType());
-            if (!SodiumExtraClientMod.options().particleSettings.otherMap.getOrDefault(particleTypeId, true)) {
-                callbackInfo.setReturnValue(null);
-            }
-        } else {
-            callbackInfo.setReturnValue(null);
-        }
-    }
 }
