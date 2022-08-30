3e9457d4270e407e7111a76f617f9bb4728100bb
Yao Chung Hu <30311066+FlashyReese@users.noreply.github.com>
2022-08-30 22:23:02 +0800

    change: Cleanup fog slider implementation

--- src/main/java/me/flashyreese/mods/sodiumextra/mixin/fog/MixinBackgroundRenderer.java
+++ src/main/java/me/flashyreese/mods/sodiumextra/mixin/fog/MixinBackgroundRenderer.java
@@ -2,15 +2,8 @@ package me.flashyreese.mods.sodiumextra.mixin.fog;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 import me.flashyreese.mods.sodiumextra.client.SodiumExtraClientMod;
-import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.render.BackgroundRenderer;
 import net.minecraft.client.render.Camera;
-import net.minecraft.client.render.CameraSubmersionType;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.effect.StatusEffects;
-import net.minecraft.util.math.MathHelper;
-import net.minecraft.world.biome.Biome;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -19,96 +12,25 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(BackgroundRenderer.class)
 public class MixinBackgroundRenderer {
 
-    @Inject(method = "applyFog", at = @At(value = "HEAD"), cancellable = true)
-    private static void applyFog(Camera camera, BackgroundRenderer.FogType fogType, float viewDistance, boolean thickFog, CallbackInfo info) {
-        if (SodiumExtraClientMod.options().renderSettings.fogDistance != 0) {
-            vanillaApplyFog(camera, fogType, viewDistance, thickFog);
-            info.cancel();
+    @Inject(method = "applyFog", at = @At(value = "TAIL"))
+    private static void applyFog(Camera camera, BackgroundRenderer.FogType fogType, float viewDistance, boolean thickFog, CallbackInfo ci) {
+        if (SodiumExtraClientMod.options().renderSettings.fogDistance == 0) {
+            return;
         }
-    }
-
-    // Vanilla copy
-    private static void vanillaApplyFog(Camera camera, BackgroundRenderer.FogType fogType, float viewDistance, boolean thickFog) {
-        CameraSubmersionType cameraSubmersionType = camera.getSubmersionType();
-        Entity entity = camera.getFocusedEntity();
-        float y;
-        if (cameraSubmersionType == CameraSubmersionType.WATER) {
-            y = 192.0F;
-            if (entity instanceof ClientPlayerEntity clientPlayerEntity) {
-                y *= Math.max(0.25F, clientPlayerEntity.getUnderwaterVisibility());
-                Biome biome = clientPlayerEntity.world.getBiome(clientPlayerEntity.getBlockPos());
-                if (biome.getCategory() == Biome.Category.SWAMP) {
-                    y *= 0.85F;
-                }
+        if (thickFog || fogType == BackgroundRenderer.FogType.FOG_TERRAIN) {
+            float fogStart = 0;
+            float fogEnd = 0;
+            if (SodiumExtraClientMod.options().renderSettings.fogDistance == 33) {
+                // Terrible hack to disable the fog, also breaks fog occlusion culling
+                // Todo: Per dimension fog toggles and sliders perhaps
+                fogStart = Short.MAX_VALUE - 1;
+                fogEnd = Short.MAX_VALUE;
+            } else if (SodiumExtraClientMod.options().renderSettings.fogDistance != 0) {
+                fogStart = SodiumExtraClientMod.options().renderSettings.fogDistance * 16;
+                fogEnd = (SodiumExtraClientMod.options().renderSettings.fogDistance + 1) * 16;
             }
-
-            RenderSystem.setShaderFogStart(-8.0F);
-            RenderSystem.setShaderFogEnd(y * 0.5F);
-        } else {
-            float ab;
-            if (cameraSubmersionType == CameraSubmersionType.LAVA) {
-                if (entity.isSpectator()) {
-                    y = -8.0F;
-                    ab = viewDistance * 0.5F;
-                } else if (entity instanceof LivingEntity && ((LivingEntity) entity).hasStatusEffect(StatusEffects.FIRE_RESISTANCE)) {
-                    y = 0.0F;
-                    ab = 3.0F;
-                } else {
-                    y = 0.25F;
-                    ab = 1.0F;
-                }
-            } else if (entity instanceof LivingEntity && ((LivingEntity) entity).hasStatusEffect(StatusEffects.BLINDNESS)) {
-                int m = ((LivingEntity) entity).getStatusEffect(StatusEffects.BLINDNESS).getDuration();
-                float n = MathHelper.lerp(Math.min(1.0F, (float) m / 20.0F), viewDistance, 5.0F);
-                if (fogType == BackgroundRenderer.FogType.FOG_SKY) {
-                    y = 0.0F;
-                    ab = n * 0.8F;
-                } else {
-                    y = n * 0.25F;
-                    ab = n;
-                }
-            } else if (cameraSubmersionType == CameraSubmersionType.POWDER_SNOW) {
-                if (entity.isSpectator()) {
-                    y = -8.0F;
-                    ab = viewDistance * 0.5F;
-                } else {
-                    y = 0.0F;
-                    ab = 2.0F;
-                }
-            } else if (thickFog) {
-                if (SodiumExtraClientMod.options().renderSettings.fogDistance == 33) {
-                    // Terrible hack to disable the fog, also breaks fog occlusion culling
-                    // Todo: Per dimension fog toggles and sliders perhaps
-                    y = Short.MAX_VALUE - 1;
-                    ab = Short.MAX_VALUE;
-                } else if (SodiumExtraClientMod.options().renderSettings.fogDistance != 0) {
-                    y = SodiumExtraClientMod.options().renderSettings.fogDistance * 16;
-                    ab = (SodiumExtraClientMod.options().renderSettings.fogDistance + 1) * 16;
-                } else {
-                    y = viewDistance * 0.05F;
-                    ab = Math.min(viewDistance, 192.0F) * 0.5F;
-                }
-            } else if (fogType == BackgroundRenderer.FogType.FOG_SKY) {
-                y = 0.0F;
-                ab = viewDistance;
-            } else {
-                if (SodiumExtraClientMod.options().renderSettings.fogDistance == 33) {
-                    // Terrible hack to disable the fog, also breaks fog occlusion culling
-                    // Todo: Per dimension fog toggles and sliders perhaps
-                    y = Short.MAX_VALUE - 1;
-                    ab = Short.MAX_VALUE;
-                } else if (SodiumExtraClientMod.options().renderSettings.fogDistance != 0) {
-                    y = SodiumExtraClientMod.options().renderSettings.fogDistance * 16;
-                    ab = (SodiumExtraClientMod.options().renderSettings.fogDistance + 1) * 16;
-                } else {
-                    y = viewDistance * 0.75F;
-                    ab = viewDistance;
-                }
-            }
-
-            RenderSystem.setShaderFogStart(y);
-            RenderSystem.setShaderFogEnd(ab);
+            RenderSystem.setShaderFogStart(fogStart);
+            RenderSystem.setShaderFogEnd(fogEnd);
         }
-
     }
 }
