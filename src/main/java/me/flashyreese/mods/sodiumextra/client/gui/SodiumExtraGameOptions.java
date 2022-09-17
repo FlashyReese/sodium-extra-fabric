a241a77aaced8e2110aab2205da9d6ade298317c
Yao Chung Hu <30311066+FlashyReese@users.noreply.github.com>
2022-09-18 02:12:17 +0800

    new: Add Linear Flat Color Blender
    Requested in Sodium issue tracker

--- src/main/java/me/flashyreese/mods/sodiumextra/client/gui/SodiumExtraGameOptions.java
+++ src/main/java/me/flashyreese/mods/sodiumextra/client/gui/SodiumExtraGameOptions.java
@@ -206,6 +206,7 @@ public class SodiumExtraGameOptions {
         public boolean multiDimensionFogControl;
         @SerializedName("dimensionFogDistance")
         public Map<Identifier, Integer> dimensionFogDistanceMap;
+        public boolean useLinearFlatColorBlender;
         public boolean lightUpdates;
         public boolean itemFrame;
         public boolean armorStand;
@@ -220,6 +221,7 @@ public class SodiumExtraGameOptions {
             this.fogDistance = 0;
             this.multiDimensionFogControl = false;
             this.dimensionFogDistanceMap = new Object2IntArrayMap<>();
+            this.useLinearFlatColorBlender = false;
             this.lightUpdates = true;
             this.itemFrame = true;
             this.armorStand = true;
