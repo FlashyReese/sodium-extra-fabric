50dbbc7cf57a84bb2d7cb8b65d9f43d4111c9680
Yao Chung Hu <30311066+FlashyReese@users.noreply.github.com>
2022-09-18 02:12:17 +0800

    new: Add Linear Flat Color Blender
    Requested in Sodium issue tracker

---
+++ src/main/java/me/flashyreese/mods/sodiumextra/mixin/sodium/biome_blend/MixinChunkRenderCache.java
@@ -0,0 +1,23 @@
+package me.flashyreese.mods.sodiumextra.mixin.sodium.biome_blend;
+
+import me.flashyreese.mods.sodiumextra.client.SodiumExtraClientMod;
+import me.flashyreese.mods.sodiumextra.client.render.terrain.color.LinearFlatColorBlender;
+import me.jellysquid.mods.sodium.client.model.quad.blender.ColorBlender;
+import me.jellysquid.mods.sodium.client.model.quad.blender.FlatColorBlender;
+import me.jellysquid.mods.sodium.client.model.quad.blender.LinearColorBlender;
+import me.jellysquid.mods.sodium.client.render.pipeline.ChunkRenderCache;
+import net.minecraft.client.MinecraftClient;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+@Mixin(value = ChunkRenderCache.class, remap = false)
+public class MixinChunkRenderCache {
+    /**
+     * @author FlashyReese
+     * @reason Includes linear flat color blender
+     */
+    @Overwrite
+    protected ColorBlender createBiomeColorBlender() {
+        return MinecraftClient.getInstance().options.biomeBlendRadius <= 0 ? new FlatColorBlender() : SodiumExtraClientMod.options().renderSettings.useLinearFlatColorBlender ? new LinearFlatColorBlender() : new LinearColorBlender();
+    }
+}
