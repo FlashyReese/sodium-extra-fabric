5462510917baaf8d5943bafd6e4d974639336be4
Yao Chung Hu <30311066+FlashyReese@users.noreply.github.com>
2022-11-20 04:02:04 +0800

    Fix fog and fireworks particle crash
    Resolves #283 and #284

--- src/main/java/me/flashyreese/mods/sodiumextra/mixin/fog/MixinBackgroundRenderer.java
+++ src/main/java/me/flashyreese/mods/sodiumextra/mixin/fog/MixinBackgroundRenderer.java
@@ -25,7 +25,8 @@ public abstract class MixinBackgroundRenderer {
     @Inject(method = "applyFog", at = @At(value = "TAIL"))
     private static void applyFog(Camera camera, BackgroundRenderer.FogType fogType, float viewDistance, boolean thickFog, float tickDelta, CallbackInfo ci) {
         Entity entity = camera.getFocusedEntity();
-        int fogDistance = SodiumExtraClientMod.options().renderSettings.multiDimensionFogControl ? SodiumExtraClientMod.options().renderSettings.dimensionFogDistanceMap.putIfAbsent(entity.world.getDimension().effects(), 0) : SodiumExtraClientMod.options().renderSettings.fogDistance;
+        SodiumExtraClientMod.options().renderSettings.dimensionFogDistanceMap.putIfAbsent(entity.world.getDimension().effects(), 0);
+        int fogDistance = SodiumExtraClientMod.options().renderSettings.multiDimensionFogControl ? SodiumExtraClientMod.options().renderSettings.dimensionFogDistanceMap.get(entity.world.getDimension().effects()) : SodiumExtraClientMod.options().renderSettings.fogDistance;
         BackgroundRenderer.StatusEffectFogModifier statusEffectFogModifier = getFogModifier(entity, tickDelta);
         if (fogDistance == 0 || statusEffectFogModifier != null) {
             return;
