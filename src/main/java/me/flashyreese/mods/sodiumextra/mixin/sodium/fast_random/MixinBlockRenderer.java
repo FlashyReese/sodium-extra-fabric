aa696896b58b82a285d20d88845e604d3a29f8fd
Yao Chung Hu <30311066+FlashyReese@users.noreply.github.com>
2022-11-24 19:05:47 +0800

    change: Don't remap some targets

--- src/main/java/me/flashyreese/mods/sodiumextra/mixin/sodium/fast_random/MixinBlockRenderer.java
+++ src/main/java/me/flashyreese/mods/sodiumextra/mixin/sodium/fast_random/MixinBlockRenderer.java
@@ -15,7 +15,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Random;
 
-@Mixin(BlockRenderer.class)
+@Mixin(value = BlockRenderer.class, remap = false)
 public class MixinBlockRenderer {
     @Shadow
     @Final
