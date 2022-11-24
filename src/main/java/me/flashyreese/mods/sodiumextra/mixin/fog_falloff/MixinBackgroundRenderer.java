2a9a23c49428152b90096d823a0c99c3cd448ab1
Yao Chung Hu <30311066+FlashyReese@users.noreply.github.com>
2022-11-24 19:05:47 +0800

    change: Don't remap some targets

--- src/main/java/me/flashyreese/mods/sodiumextra/mixin/fog_falloff/MixinBackgroundRenderer.java
+++ src/main/java/me/flashyreese/mods/sodiumextra/mixin/fog_falloff/MixinBackgroundRenderer.java
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(BackgroundRenderer.class)
 public class MixinBackgroundRenderer {
-    @ModifyArg(method = "applyFog", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;setShaderFogStart(F)V"), index = 0)
+    @ModifyArg(method = "applyFog", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;setShaderFogStart(F)V", remap = false), index = 0)
     private static float modifySetShaderFogStart(float original) {
         float fogStart = (float) SodiumExtraClientMod.options().renderSettings.fogStart / 100;
         return original * fogStart;
