a9413fa9d3fe32b49a3c426687a49f5d627c3c14
Europa_You <v1europayou@gmail.com>
2023-01-25 20:58:41 +0300

    Added caching to HudRenderImpl

--- src/main/java/me/flashyreese/mods/sodiumextra/client/gui/HudRenderImpl.java
+++ src/main/java/me/flashyreese/mods/sodiumextra/client/gui/HudRenderImpl.java
@@ -12,10 +12,17 @@ import net.minecraft.util.math.Vec3d;
 public class HudRenderImpl implements HudRenderCallback {
 
     private final MinecraftClient client = MinecraftClient.getInstance();
+    public SodiumExtraGameOptions.OverlayCorner overlayCorner;
+    int fontHeight, scaledWidth, scaledHeight, fpsTextWidth, coordsTextWidth, lightUpdatesTextWidth;
 
     @Override
     public void onHudRender(MatrixStack matrixStack, float tickDelta) {
         if (!this.client.options.debugEnabled) {
+            overlayCorner = SodiumExtraClientMod.options().extraSettings.overlayCorner;
+            fontHeight = this.client.textRenderer.fontHeight;
+            scaledWidth = this.client.getWindow().getScaledWidth();
+            scaledHeight = this.client.getWindow().getScaledHeight();
+
             //Gotta love hardcoding
             if (SodiumExtraClientMod.options().extraSettings.showFps && SodiumExtraClientMod.options().extraSettings.showCoords) {
                 this.renderFPS(matrixStack);
@@ -40,100 +47,102 @@ public class HudRenderImpl implements HudRenderCallback {
         if (SodiumExtraClientMod.options().extraSettings.showFPSExtended)
             text = Text.literal(String.format("%s %s", text.getString(), Text.translatable("sodium-extra.overlay.fps_extended", SodiumExtraClientMod.getClientTickHandler().getHighestFps(), SodiumExtraClientMod.getClientTickHandler().getAverageFps(),
                     SodiumExtraClientMod.getClientTickHandler().getLowestFps()).getString()));
+        fpsTextWidth = this.client.textRenderer.getWidth(text);
 
         int x, y;
-        switch (SodiumExtraClientMod.options().extraSettings.overlayCorner) {
+        switch (overlayCorner) {
             case TOP_LEFT -> {
                 x = 2;
                 y = 2;
             }
             case TOP_RIGHT -> {
-                x = this.client.getWindow().getScaledWidth() - this.client.textRenderer.getWidth(text) - 2;
+                x = scaledWidth - fpsTextWidth - 2;
                 y = 2;
             }
             case BOTTOM_LEFT -> {
                 x = 2;
-                y = this.client.getWindow().getScaledHeight() - this.client.textRenderer.fontHeight - 2;
+                y = scaledHeight - fontHeight - 2;
             }
             case BOTTOM_RIGHT -> {
-                x = this.client.getWindow().getScaledWidth() - this.client.textRenderer.getWidth(text) - 2;
-                y = this.client.getWindow().getScaledHeight() - this.client.textRenderer.fontHeight - 2;
+                x = scaledWidth - fpsTextWidth - 2;
+                y = scaledHeight - fontHeight - 2;
             }
             default ->
-                    throw new IllegalStateException("Unexpected value: " + SodiumExtraClientMod.options().extraSettings.overlayCorner);
+                    throw new IllegalStateException("Unexpected value: " + overlayCorner);
         }
 
-        this.drawString(matrices, text, x, y);
+        this.drawString(matrices, text, x, y, fpsTextWidth);
     }
 
     private void renderCoords(MatrixStack matrices) {
         if (this.client.player == null) return;
-        if (this.client.hasReducedDebugInfo()) return;
         Vec3d pos = this.client.player.getPos();
 
         Text text = Text.translatable("sodium-extra.overlay.coordinates", String.format("%.2f", pos.x), String.format("%.2f", pos.y), String.format("%.2f", pos.z));
+        coordsTextWidth =  this.client.textRenderer.getWidth(text);
 
         int x, y;
-        switch (SodiumExtraClientMod.options().extraSettings.overlayCorner) {
+        switch (overlayCorner) {
             case TOP_LEFT -> {
                 x = 2;
                 y = 12;
             }
             case TOP_RIGHT -> {
-                x = this.client.getWindow().getScaledWidth() - this.client.textRenderer.getWidth(text) - 2;
+                x = scaledWidth - coordsTextWidth - 2;
                 y = 12;
             }
             case BOTTOM_LEFT -> {
                 x = 2;
-                y = this.client.getWindow().getScaledHeight() - this.client.textRenderer.fontHeight - 12;
+                y = scaledHeight - fontHeight - 12;
             }
             case BOTTOM_RIGHT -> {
-                x = this.client.getWindow().getScaledWidth() - this.client.textRenderer.getWidth(text) - 2;
-                y = this.client.getWindow().getScaledHeight() - this.client.textRenderer.fontHeight - 12;
+                x = scaledWidth - coordsTextWidth - 2;
+                y = scaledHeight - fontHeight - 12;
             }
             default ->
-                    throw new IllegalStateException("Unexpected value: " + SodiumExtraClientMod.options().extraSettings.overlayCorner);
+                    throw new IllegalStateException("Unexpected value: " + overlayCorner);
         }
 
-        this.drawString(matrices, text, x, y);
+        this.drawString(matrices, text, x, y, coordsTextWidth);
     }
 
     private void renderLightUpdatesWarning(MatrixStack matrices) {
         Text text = Text.translatable("sodium-extra.overlay.light_updates");
+        lightUpdatesTextWidth = this.client.textRenderer.getWidth(text);
 
         int x, y;
-        switch (SodiumExtraClientMod.options().extraSettings.overlayCorner) {
+        switch (overlayCorner) {
             case TOP_LEFT -> {
                 x = 2;
                 y = 22;
             }
             case TOP_RIGHT -> {
-                x = this.client.getWindow().getScaledWidth() - this.client.textRenderer.getWidth(text) - 2;
+                x = scaledWidth - lightUpdatesTextWidth - 2;
                 y = 22;
             }
             case BOTTOM_LEFT -> {
                 x = 2;
-                y = this.client.getWindow().getScaledHeight() - this.client.textRenderer.fontHeight - 22;
+                y = scaledHeight - fontHeight - 22;
             }
             case BOTTOM_RIGHT -> {
-                x = this.client.getWindow().getScaledWidth() - this.client.textRenderer.getWidth(text) - 2;
-                y = this.client.getWindow().getScaledHeight() - this.client.textRenderer.fontHeight - 22;
+                x = scaledWidth - lightUpdatesTextWidth - 2;
+                y = scaledHeight - fontHeight - 22;
             }
             default ->
-                    throw new IllegalStateException("Unexpected value: " + SodiumExtraClientMod.options().extraSettings.overlayCorner);
+                    throw new IllegalStateException("Unexpected value: " + overlayCorner);
         }
 
-        this.drawString(matrices, text, x, y);
+        this.drawString(matrices, text, x, y, lightUpdatesTextWidth);
     }
 
-    private void drawString(MatrixStack matrices, Text text, int x, int y) {
-        if (SodiumExtraClientMod.options().extraSettings.textContrast == SodiumExtraGameOptions.TextContrast.NONE) {
-            this.client.textRenderer.draw(matrices, text, x, y, 0xffffffff);
-        } else if (SodiumExtraClientMod.options().extraSettings.textContrast == SodiumExtraGameOptions.TextContrast.BACKGROUND) {
-            DrawableHelper.fill(matrices, x - 1, y - 1, x + this.client.textRenderer.getWidth(text) + 1, y + this.client.textRenderer.fontHeight, -1873784752);
-            this.client.textRenderer.draw(matrices, text, x, y, 0xffffffff);
-        } else if (SodiumExtraClientMod.options().extraSettings.textContrast == SodiumExtraGameOptions.TextContrast.SHADOW) {
-            this.client.textRenderer.drawWithShadow(matrices, text, x, y, 0xffffffff);
+    private void drawString(MatrixStack matrices, Text text, int x, int y, int textWidth) {
+        switch (SodiumExtraClientMod.options().extraSettings.textContrast) {
+            case NONE -> this.client.textRenderer.draw(matrices, text, x, y, 0xffffffff);
+            case BACKGROUND -> {
+                DrawableHelper.fill(matrices, x - 1, y - 1, x + textWidth + 1, y + fontHeight, -1873784752);
+                this.client.textRenderer.draw(matrices, text, x, y, 0xffffffff);
+            }
+            case SHADOW -> this.client.textRenderer.drawWithShadow(matrices, text, x, y, 0xffffffff);
         }
     }
 }
