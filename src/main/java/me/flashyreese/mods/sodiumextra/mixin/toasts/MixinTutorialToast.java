b07c9e33ca74410ac0d365d4c09f4fc5d6967be2
Yao Chung Hu <30311066+FlashyReese@users.noreply.github.com>
2023-06-08 00:54:38 +0800

    change: Fix toast notifications mixins injection
    some how I didn't catch this and the toolchain just let me compile it and use it.

--- src/main/java/me/flashyreese/mods/sodiumextra/mixin/toasts/MixinTutorialToast.java
+++ src/main/java/me/flashyreese/mods/sodiumextra/mixin/toasts/MixinTutorialToast.java
@@ -1,10 +1,10 @@
 package me.flashyreese.mods.sodiumextra.mixin.toasts;
 
 import me.flashyreese.mods.sodiumextra.client.SodiumExtraClientMod;
+import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.toast.Toast;
 import net.minecraft.client.toast.ToastManager;
 import net.minecraft.client.toast.TutorialToast;
-import net.minecraft.client.util.math.MatrixStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -13,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(TutorialToast.class)
 public class MixinTutorialToast {
     @Inject(method = "draw", at = @At("HEAD"), cancellable = true)
-    public void draw(MatrixStack matrices, ToastManager manager, long startTime, CallbackInfoReturnable<Toast.Visibility> cir) {
+    public void draw(DrawContext context, ToastManager manager, long startTime, CallbackInfoReturnable<Toast.Visibility> cir) {
         if (!SodiumExtraClientMod.options().extraSettings.tutorialToast) {
             cir.setReturnValue(Toast.Visibility.HIDE);
         }
