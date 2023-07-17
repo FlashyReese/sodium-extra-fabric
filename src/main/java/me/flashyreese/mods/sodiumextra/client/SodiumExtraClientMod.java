ccd788b898407d55917d28d0550e84336afd052c
Yao Chung Hu <30311066+FlashyReese@users.noreply.github.com>
2023-07-17 19:33:51 +0800

    change: Disable options like if mixin option is disabled

--- src/main/java/me/flashyreese/mods/sodiumextra/client/SodiumExtraClientMod.java
+++ src/main/java/me/flashyreese/mods/sodiumextra/client/SodiumExtraClientMod.java
@@ -3,6 +3,7 @@ package me.flashyreese.mods.sodiumextra.client;
 import de.guntram.mcmod.crowdintranslate.CrowdinTranslate;
 import me.flashyreese.mods.sodiumextra.client.gui.HudRenderImpl;
 import me.flashyreese.mods.sodiumextra.client.gui.SodiumExtraGameOptions;
+import net.caffeinemc.caffeineconfig.CaffeineConfig;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
@@ -16,6 +17,7 @@ public class SodiumExtraClientMod implements ClientModInitializer {
 
     private static final ClientTickHandler clientTickHandler = new ClientTickHandler();
     private static SodiumExtraGameOptions CONFIG;
+    private static CaffeineConfig MIXIN_CONFIG;
     private static Logger LOGGER;
 
     public static Logger logger() {
@@ -32,6 +34,48 @@ public class SodiumExtraClientMod implements ClientModInitializer {
         }
 
         return CONFIG;
+    }
+
+    public static CaffeineConfig mixinConfig() {
+        if (MIXIN_CONFIG == null) {
+            MIXIN_CONFIG = CaffeineConfig.builder("Sodium Extra").withSettingsKey("sodium-extra:options")
+                    .addMixinOption("adaptive_sync", true)
+                    .addMixinOption("animation", true)
+                    .addMixinOption("biome_colors", true)
+                    .addMixinOption("cloud", true)
+                    .addMixinOption("compat", true, false)
+                    .addMixinOption("fog", true)
+                    .addMixinOption("fog_falloff", true)
+                    .addMixinOption("gui", true)
+                    .addMixinOption("instant_sneak", true)
+                    .addMixinOption("light_updates", true)
+                    .addMixinOption("optimizations", true)
+                    .addMixinOption("optimizations.beacon_beam_rendering", true)
+                    .addMixinOption("particle", true)
+                    .addMixinOption("prevent_shaders", true)
+                    .addMixinOption("reduce_resolution_on_mac", true)
+                    .addMixinOption("render", true)
+                    .addMixinOption("render.block", true)
+                    .addMixinOption("render.block.entity", true)
+                    .addMixinOption("render.entity", true)
+                    .addMixinOption("sky", true)
+                    .addMixinOption("sky_colors", true)
+                    .addMixinOption("sodium", true)
+                    .addMixinOption("sodium.accessibility", true)
+                    .addMixinOption("sodium.biome_blend", true)
+                    .addMixinOption("sodium.resolution", true)
+                    .addMixinOption("sodium.scrollable_page", true)
+                    .addMixinOption("sodium.vsync", true)
+                    .addMixinOption("stars", true)
+                    .addMixinOption("steady_debug_hud", true)
+                    .addMixinOption("sun_moon", true)
+                    .addMixinOption("toasts", true)
+
+                    //.withLogger(SodiumExtraClientMod.logger())
+                    .withInfoUrl("https://github.com/FlashyReese/sodium-extra-fabric/wiki/Configuration-File")
+                    .build(FabricLoader.getInstance().getConfigDir().resolve("sodium-extra.properties"));
+        }
+        return MIXIN_CONFIG;
     }
 
     public static ClientTickHandler getClientTickHandler() {
