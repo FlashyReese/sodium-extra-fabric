b8f2b5d8b1737203e4cc495880a92c0ff1cfe1c9
Yao Chung Hu <30311066+FlashyReese@users.noreply.github.com>
2023-08-03 09:06:52 +0800

    change: Use default priority for draw helpers
    Apparently this breaks Iris but Iris doesn't even modify this class.

--- src/main/java/me/flashyreese/mods/sodiumextra/mixin/optimizations/draw_helpers/MixinDrawContext.java
+++ src/main/java/me/flashyreese/mods/sodiumextra/mixin/optimizations/draw_helpers/MixinDrawContext.java
@@ -20,7 +20,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(value = DrawContext.class, priority = 1500)
+@Mixin(DrawContext.class)
 public abstract class MixinDrawContext {
 
     @Shadow
