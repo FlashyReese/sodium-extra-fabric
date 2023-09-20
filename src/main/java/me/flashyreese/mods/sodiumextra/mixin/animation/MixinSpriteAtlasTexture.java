93756ab0ceb43e69e17d5cdcf4f847ea39c1a183
Yao Chung Hu <30311066+FlashyReese@users.noreply.github.com>
2023-09-20 23:12:23 +0800

    change: Update animation list

--- src/main/java/me/flashyreese/mods/sodiumextra/mixin/animation/MixinSpriteAtlasTexture.java
+++ src/main/java/me/flashyreese/mods/sodiumextra/mixin/animation/MixinSpriteAtlasTexture.java
@@ -10,8 +10,67 @@ import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
+import java.util.List;
+import java.util.Map;
+import java.util.function.Supplier;
+
 @Mixin(SpriteAtlasTexture.class)
 public abstract class MixinSpriteAtlasTexture extends AbstractTexture {
+    @Unique
+    private final Map<Supplier<Boolean>, List<Identifier>> animatedSprites = Map.of(
+            () -> SodiumExtraClientMod.options().animationSettings.water, List.of(
+                    new Identifier("minecraft", "block/water_still"),
+                    new Identifier("minecraft", "block/water_flow")
+            ),
+            () -> SodiumExtraClientMod.options().animationSettings.lava, List.of(
+                    new Identifier("minecraft", "block/lava_still"),
+                    new Identifier("minecraft", "block/lava_flow")
+            ),
+            () -> SodiumExtraClientMod.options().animationSettings.portal, List.of(
+                    new Identifier("minecraft", "block/nether_portal")
+            ),
+            () -> SodiumExtraClientMod.options().animationSettings.fire, List.of(
+                    new Identifier("minecraft", "block/fire_0"),
+                    new Identifier("minecraft", "block/fire_1"),
+                    new Identifier("minecraft", "block/soul_fire_0"),
+                    new Identifier("minecraft", "block/soul_fire_1"),
+                    new Identifier("minecraft", "block/campfire_fire"),
+                    new Identifier("minecraft", "block/campfire_log_lit"),
+                    new Identifier("minecraft", "block/soul_campfire_fire"),
+                    new Identifier("minecraft", "block/soul_campfire_log_lit")
+            ),
+            () -> SodiumExtraClientMod.options().animationSettings.blockAnimations, List.of(
+                    new Identifier("minecraft", "block/magma"),
+                    new Identifier("minecraft", "block/lantern"),
+                    new Identifier("minecraft", "block/sea_lantern"),
+                    new Identifier("minecraft", "block/soul_lantern"),
+                    new Identifier("minecraft", "block/kelp"),
+                    new Identifier("minecraft", "block/kelp_plant"),
+                    new Identifier("minecraft", "block/seagrass"),
+                    new Identifier("minecraft", "block/tall_seagrass_top"),
+                    new Identifier("minecraft", "block/tall_seagrass_bottom"),
+                    new Identifier("minecraft", "block/warped_stem"),
+                    new Identifier("minecraft", "block/crimson_stem"),
+                    new Identifier("minecraft", "block/blast_furnace_front_on"),
+                    new Identifier("minecraft", "block/smoker_front_on"),
+                    new Identifier("minecraft", "block/stonecutter_saw"),
+                    new Identifier("minecraft", "block/prismarine"),
+                    new Identifier("minecraft", "block/respawn_anchor_top"),
+                    new Identifier("minecraft", "entity/conduit/wind"),
+                    new Identifier("minecraft", "entity/conduit/wind_vertical")
+            ),
+            () -> SodiumExtraClientMod.options().animationSettings.sculkSensor, List.of(
+                    new Identifier("minecraft", "block/sculk"),
+                    new Identifier("minecraft", "block/sculk_catalyst_top_bloom"),
+                    new Identifier("minecraft", "block/sculk_catalyst_side_bloom"),
+                    new Identifier("minecraft", "block/sculk_shrieker_inner_top"),
+                    new Identifier("minecraft", "block/sculk_vein"),
+                    new Identifier("minecraft", "block/sculk_shrieker_can_summon_inner_top"),
+                    new Identifier("minecraft", "block/sculk_sensor_tendril_inactive"),
+                    new Identifier("minecraft", "block/sculk_sensor_tendril_active"),
+                    new Identifier("minecraft", "vibration")
+            )
+    );
 
     @Redirect(method = "upload", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/texture/Sprite;createAnimation()Lnet/minecraft/client/texture/Sprite$TickableAnimation;"))
     public Sprite.TickableAnimation sodiumExtra$tickAnimatedSprites(Sprite instance) {
@@ -24,26 +83,10 @@ public abstract class MixinSpriteAtlasTexture extends AbstractTexture {
     @Unique
     private boolean shouldAnimate(Identifier identifier) {
         if (identifier != null) {
-            String path = identifier.getPath();
-            if (path.endsWith("water_still") || path.endsWith("water_flow")) {
-                return SodiumExtraClientMod.options().animationSettings.water;
-            } else if (path.endsWith("lava_still") || path.endsWith("lava_flow")) {
-                return SodiumExtraClientMod.options().animationSettings.lava;
-            } else if (path.endsWith("nether_portal")) {
-                return SodiumExtraClientMod.options().animationSettings.portal;
-            } else if (path.endsWith("fire_0") || path.endsWith("fire_1") || path.endsWith("soul_fire_0") ||
-                    path.endsWith("soul_fire_1") || path.endsWith("campfire_fire") || path.endsWith("campfire_log_lit") ||
-                    path.endsWith("soul_campfire_fire") || path.endsWith("soul_campfire_log_lit")) {
-                return SodiumExtraClientMod.options().animationSettings.fire;
-            } else if (path.endsWith("magma") || path.endsWith("lantern") || path.endsWith("sea_lantern") ||
-                    path.endsWith("soul_lantern") || path.endsWith("kelp") || path.endsWith("kelp_plant") ||
-                    path.endsWith("seagrass") || path.endsWith("warped_stem") || path.endsWith("crimson_stem") ||
-                    path.endsWith("blast_furnace_front_on") || path.endsWith("smoker_front_on") ||
-                    path.endsWith("stonecutter_saw")) {
-                return SodiumExtraClientMod.options().animationSettings.blockAnimations;
-            } else if (path.endsWith("sculk_sensor_tendril_inactive") || path.endsWith("sculk_sensor_tendril_active") ||
-                    path.endsWith("particle/vibration")) {
-                return SodiumExtraClientMod.options().animationSettings.sculkSensor;
+            for (Map.Entry<Supplier<Boolean>, List<Identifier>> supplierListEntry : this.animatedSprites.entrySet()) {
+                if (supplierListEntry.getValue().contains(identifier)) {
+                    return supplierListEntry.getKey().get();
+                }
             }
         }
         return true;
