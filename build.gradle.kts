f92d599fad0dc20a0abc4bba6738f86083caceb6
Yao Chung Hu <reeszrbteam@gmail.com>
2026-09-13 00:50:16 +0800

    build: switch to architectury setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,34 @@
 plugins {
     id("java")
-    id("net.fabricmc.fabric-loom") version ("1.17.13") apply (false)
+    id("dev.architectury.loom-no-remap") version "1.17.483" apply false
+    id("architectury-plugin") version "3.5.169"
+    id("com.gradleup.shadow") version "9.3.0" apply false
 }
 
-val MINECRAFT_VERSION by extra { "26.2" }
-val NEOFORGE_VERSION by extra { "26.2.0.1-beta" }
-val FABRIC_LOADER_VERSION by extra { "0.19.3" }
-val FABRIC_API_VERSION by extra { "0.152.1+26.2" }
+val MINECRAFT_VERSION = "26.2"
+val NEOFORGE_VERSION = "26.2.0.1-beta"
+val FABRIC_LOADER_VERSION = "0.19.3"
+val FABRIC_API_VERSION = "0.152.1+26.2"
+val GREENLIGHT_VERSION = "0.1.0+mc26.2"
 
 // https://semver.org/
-val MAVEN_GROUP by extra { "me.flashyreese.mods" }
-val ARCHIVE_NAME by extra { "sodium-extra" }
-val MOD_VERSION by extra { "0.9.3" }
-val SODIUM_VERSION by extra { "0.9.1+mc26.2" }
-val GREENLIGHT_VERSION by extra { "0.1.0+mc26.2" }
+val MAVEN_GROUP = providers.gradleProperty("maven_group").get()
+val ARCHIVE_NAME = providers.gradleProperty("archives_name").get()
+val MOD_VERSION = "0.9.3"
+val SODIUM_VERSION = "0.9.1+mc26.2"
+
+extra["MINECRAFT_VERSION"] = MINECRAFT_VERSION
+extra["NEOFORGE_VERSION"] = NEOFORGE_VERSION
+extra["FABRIC_LOADER_VERSION"] = FABRIC_LOADER_VERSION
+extra["FABRIC_API_VERSION"] = FABRIC_API_VERSION
+extra["SODIUM_VERSION"] = SODIUM_VERSION
+extra["GREENLIGHT_VERSION"] = GREENLIGHT_VERSION
+
+architectury {
+    minecraft = MINECRAFT_VERSION
+}
 
 allprojects {
-    apply(plugin = "java")
-    apply(plugin = "maven-publish")
     group = MAVEN_GROUP
     version = createVersionString()
 }
@@ -27,16 +38,16 @@ tasks.withType<JavaCompile> {
 }
 
 subprojects {
-    val modVersion = createVersionString()
-
+    apply(plugin = "java")
     apply(plugin = "maven-publish")
 
     repositories {
-        maven("https://maven.parchmentmc.org/")
+        mavenCentral()
+        maven("https://maven.fabricmc.net/")
+        maven("https://api.modrinth.com/maven")
+        maven("https://libraries.minecraft.net/")
         maven("https://maven.caffeinemc.net/releases")
         maven("https://maven.caffeinemc.net/snapshots")
-        maven("https://api.modrinth.com/maven")
-        maven("https://libraries.minecraft.net")
         maven("https://maven.flashyreese.me/releases")
         maven("https://maven.flashyreese.me/snapshots")
     }
@@ -46,18 +57,6 @@ subprojects {
     }
 
     java.toolchain.languageVersion = JavaLanguageVersion.of(25)
-
-    tasks.processResources {
-        inputs.property("version", modVersion)
-        inputs.property("minecraft_version", MINECRAFT_VERSION)
-
-        filesMatching("META-INF/neoforge.mods.toml") {
-            expand(mapOf("version" to modVersion, "minecraft_version" to MINECRAFT_VERSION))
-        }
-    }
-
-    version = modVersion
-    group = "me.flashyreese.mods"
 
     tasks.withType<JavaCompile> {
         options.encoding = "UTF-8"
--- gradlew.bat
+++ gradlew.bat
@@ -19,7 +19,7 @@
 @if "%DEBUG%"=="" @echo off
 @rem ##########################################################################
 @rem
-@rem  Gradle startup script for Windows
+@rem  gradlew startup script for Windows
 @rem
 @rem ##########################################################################
 
@@ -36,7 +36,7 @@ set APP_HOME=%DIRNAME%
 for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
 
 @rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
-set DEFAULT_JVM_OPTS="-Dfile.encoding=UTF-8 -Xmx64m" "-Xms64m"
+set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
 
 @rem Find java.exe
 if defined JAVA_HOME goto findJavaFromJavaHome
@@ -72,7 +72,7 @@ echo location of your Java installation. 1>&2
 
 
 
-@rem Execute Gradle
+@rem Execute gradlew
 @rem endlocal doesn't take effect until after the line is parsed and variables are expanded
 @rem which allows us to clear the local environment before executing the java command
 endlocal & "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -jar "%APP_HOME%\gradle\wrapper\gradle-wrapper.jar" %* & call :exitWithErrorLevel
